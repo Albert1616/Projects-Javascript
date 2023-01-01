addca908e522801c09bb7b635f707dbab60800ed
Matheus Albert <matheusal2018@outlook.com>
2023-01-01 17:34:19 -0300

    HTML/CSS parcialmente finalizado

---
+++ calculadora/js/script.js
@@ -0,0 +1,5 @@
+const previous = document.querySelector(".previous-operation");
+const current = document.querySelector(".current-operation");
+const button = document.querySelectorAll("#number-box button");
+console.log(button);
+
