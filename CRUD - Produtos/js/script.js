aaac995d0266b64fd7073e2f597c652fef86c393
Matheus Albert <matheusal2018@outlook.com>
2023-01-07 20:54:43 -0300

    HTML + CSS parcialmente prontos

---
+++ CRUD - Produtos/js/script.js
@@ -0,0 +1,7 @@
+const form = document.querySelector(".form_newproduct");
+function mostrarForm(){
+    form.style.display = "block";
+}
+function esconderForm(){
+    form.style.display = "none";
+}
